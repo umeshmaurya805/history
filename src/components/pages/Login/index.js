7fdc79595aa4bcf977d7b3041f161dbee4c46e08
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-06-14 15:22:11 +0530

    Login Component Added

---
+++ src/components/pages/Login/index.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Typography from "@material-ui/core/Typography";
+import Checkbox from "@material-ui/core/Checkbox";
+import { Link } from "react-router-dom";
+// import Paper from "@material-ui/core/Paper";
+// import Grid from "@material-ui/core/Grid";
+import useStyles from "./style";
+
+const Login = ({ showForgotPasswordScreen }) => {
+  const classes = useStyles();
+  const [school, setSchool] = useState({email: '', password: ''});
+
+  const handleChange = (e) => {
+    setSchool({ ...school, [e.target.name]: e.target.value });
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(school);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Typography component="h3" variant="h3" className={classes.title}>
+        Login
+      </Typography>
+      <Typography component="p" variant="subtitle1" className={classes.subTitle}>
+        Login with the data provided during your registration.
+      </Typography>
+      <form className={classes.form} onChange={handleChange} onSubmit={handleFormSubmit}>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email"
+          name="email"
+          autoComplete="email"
+          autoFocus
+          value={school.email}
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          value={school.password}
+        />
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+        >
+          Log in
+        </Button>
+        <div className={classes.forgotPassword}>
+          <Link to="/forgot-password">{"Did you forget your password?"}</Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/pages/Login/style.js
@@ -0,0 +1,48 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: "auto",
+      marginRight: "auto",
+      padding: theme.spacing(4),
+    },
+  },
+  title: {
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "500",
+    color: theme.palette.primary.main,
+    marginBottom: "10px",
+  },
+  subTitle: {
+    fontFamily: "'Inter', sans-serif",
+    marginBottom: "20px",
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  forgotPassword: {
+    display: "flex",
+    justifyContent: "flex-end",
+
+    "& a": {
+      textDecoration: "none",
+      color: theme.palette.primary.main,
+    },
+    "& a:hover": {
+      opacity: "0.8",
+    },
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    height: "56px",
+  },
+}));
+
+export default useStyles;
