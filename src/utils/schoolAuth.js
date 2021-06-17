33a56548be761fa247ce92e5ee41997dd55eeff0
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-06-17 18:57:28 +0530

    Added school auth

---
+++ src/utils/schoolAuth.js
@@ -0,0 +1,26 @@
+export const saveSchoolAuth = (data) => {
+  localStorage.setItem(
+    `${process.env.REACT_APP_PREFIX}-schoolAuth`,
+    JSON.stringify(data)
+  );
+};
+
+export const removeSchoolAuth = () => {
+  localStorage.removeItem(`${process.env.REACT_APP_PREFIX}-schoolAuth`);
+};
+
+export const getSchoolAuth = () => {
+  let data = localStorage.getItem(`${process.env.REACT_APP_PREFIX}-schoolAuth`);
+
+  const getInitialState = () => ({
+    accessToken: null,
+    expiresAt: Date.now(),
+    isAuthenticated: false,
+  });
+
+  if (!data) return getInitialState();
+  
+  data = JSON.parse(data);
+
+  return data.expiresAt > Date.now() ? data : getInitialState();
+};
