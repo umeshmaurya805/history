abae9098d07d52062e5c4cba9b45b9c9f5118dbf
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-06-19 12:12:54 +0530

    New Loading Button added

---
+++ src/components/common/LoadingButton/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+const LoadingButton = ({ isLoading, children, ...props }) => {
+  return (
+    <Button {...props}>
+      {isLoading ? <CircularProgress color="#FFF" /> : children}
+    </Button>
+  );
+};
+
+export default LoadingButton;
