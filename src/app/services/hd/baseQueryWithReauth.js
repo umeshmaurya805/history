8fb8531af8de068cf8865757ab2524e9a2e0428e
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-06-21 18:11:02 +0530

    updated package

--- src/app/services/hd/baseQueryWithReauth.js
+++ src/app/services/hd/baseQueryWithReauth.js
@@ -6,7 +6,7 @@ import {
 } from "../../../utils/schoolAuth";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: `${process.env.REACT_APP_SERVER_URL}`,
+  baseUrl: `${process.env.REACT_APP_SERVER_URL}/api`,
   credentials: "include", // Required to use cookies
   prepareHeaders: (headers) => {
     // By default, if we have a token in the store, let's use that for authenticated requests
