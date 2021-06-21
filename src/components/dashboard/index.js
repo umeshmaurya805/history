b5a054d10b8d8286b1dc86fa20cc051afb0eaa04
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-06-21 18:17:40 +0530

    removed warnings

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,34 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 import cx from "clsx";
-import Toolbar from "@material-ui/core/Toolbar";
 import {
   Root,
-  getHeader,
   getDrawerSidebar,
-  getSidebarTrigger,
   getSidebarContent,
   getCollapseBtn,
   getContent,
-  getFooter,
   getFixedScheme,
 } from "@mui-treasury/layout";
-import {
-  HeaderMockUp,
-  ContentMockUp,
-  FooterMockUp,
-  // NavContentMockUp,
-} from "@mui-treasury/mockup/layout";
 import { TextSidebar } from '@mui-treasury/mockup/sidebars';
 import useStyles from "./style";
 
-const Header = getHeader(styled);
 const DrawerSidebar = getDrawerSidebar(styled);
-const SidebarTrigger = getSidebarTrigger(styled);
 const SidebarContent = getSidebarContent(styled);
 const CollapseBtn = getCollapseBtn(styled);
 const Content = getContent(styled);
-const Footer = getFooter(styled);
 
 const fixedScheme = getFixedScheme();
 
@@ -47,7 +34,6 @@ const Dashboard = () => {
         <CollapseBtn className={cx(styles.collapseBtn)} />
       </DrawerSidebar>
       <Content className={styles.content}>
-        {/* <ContentMockUp /> */}
       </Content>
     </Root>
   );
