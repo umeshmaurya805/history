67c9aca518309e628de51a4492199d55d45d4c7a
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-06-22 14:43:01 +0530

    added calendar

---
+++ src/components/dashboard/Calendar/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import LoadingButton from "../../common/LoadingButton";
+import { useLogoutMutation } from "../../../app/services/hd/auth";
+import { useGetProfileQuery } from "../../../app/services/hd/school";
+import protectedHandler from "../../../utils/protectedHandler";
+import { removeSchoolAuth } from "../../../utils/schoolAuth";
+
+const Calendar = () => {
+  const [logout, { isLoading: isLoggingOut }] = useLogoutMutation();
+  const { data, isLoading } = useGetProfileQuery();
+  const history = useHistory();
+
+  const logoutSchool = protectedHandler(async () => {
+    await logout().unwrap();
+    removeSchoolAuth();
+    history.push("/auth/login");
+  });
+
+  return (
+    <div>
+      <LoadingButton
+        variant="contained"
+        color="primary"
+        onClick={logoutSchool}
+        isLoading={isLoggingOut}
+      >
+        Logout
+      </LoadingButton>
+      Calendar
+      {isLoading ? "Loading" : data.email}
+    </div>
+  );
+};
+
+export default Calendar;
