a5fe925477ef974c5d8f7bebc1cf43dfddfe8c49
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-06-23 11:24:27 +0530

    analytics icon added

---
+++ src/components/common/Icons/AnalyticsIcon.js
@@ -0,0 +1,29 @@
+import React from "react";
+import icon from "../../../assets/svg/analytics-icon.svg";
+import selectedIcon from "../../../assets/svg/analytics-icon-selected.svg";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    padding: "4px 0 3px 2px",
+    borderLeft: "2px solid white",
+    borderBottom: "2px solid white",
+  },rootSelected: {
+    padding: "4px 0 3px 2px",
+    borderLeft: "2px solid #4A90E2",
+    borderBottom: "2px solid #4A90E2",
+  },
+}));
+
+const AnalyticsIcon = ({ isSelected }) => {
+  const classes = useStyles();
+  return (
+    <img
+      className={isSelected ? classes.rootSelected : classes.root}
+      src={isSelected ? selectedIcon : icon}
+      alt=""
+    />
+  );
+};
+
+export default AnalyticsIcon;
