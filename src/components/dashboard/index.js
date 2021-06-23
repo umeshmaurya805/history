0539e7091a458ada666a92dba769dcd410c2534c
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-06-23 11:27:22 +0530

    updated dashboard

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,60 +1,23 @@
 import React from "react";
-import styled from "styled-components";
-import {
-  getDrawerSidebar,
-  getSidebarContent,
-  getContent,
-  getHeader,
-  getSidebarTrigger,
-} from "@mui-treasury/layout";
-import Toolbar from "@material-ui/core/Toolbar";
 import { Switch, Route, Redirect, useRouteMatch } from "react-router-dom";
 import Home from "./Home";
-import { HeaderMockUp } from "@mui-treasury/mockup/layout";
-import useStyles from "./style";
-import NavSidebar from "./NavSidebar";
 import MyAccount from "./MyAccount";
 import Calendar from "./Calendar";
 import Analytics from "./Analytics";
 import Leaderboard from "./Leaderboard";
 
-const Header = getHeader(styled);
-const DrawerSidebar = getDrawerSidebar(styled);
-const SidebarTrigger = getSidebarTrigger(styled);
-const SidebarContent = getSidebarContent(styled);
-const Content = getContent(styled);
-
 const Dashboard = () => {
-  const styles = useStyles();
   const { path } = useRouteMatch();
 
   return (
-    <React.Fragment>
-      <Header className={styles.header}>
-        <Toolbar>
-          <SidebarTrigger sidebarId={"primarySidebar"} />
-          <HeaderMockUp />
-        </Toolbar>
-      </Header>
-      <DrawerSidebar
-        sidebarId={"primarySidebar"}
-        PaperProps={{ className: styles.sidebar }}
-      >
-        <SidebarContent>
-          <NavSidebar />
-        </SidebarContent>
-      </DrawerSidebar>
-      <Content className={styles.content}>
-        <Switch>
-          <Route exact path={`${path}/home`} component={Home} />
-          <Route exact path={`${path}/calendar`} component={Calendar} />
-          <Route exact path={`${path}/analytics`} component={Analytics} />
-          <Route exact path={`${path}/leaderboard`} component={Leaderboard} />
-          <Route exact path={`${path}/my-account`} component={MyAccount} />
-          <Redirect to={`${path}/home`} />
-        </Switch>
-      </Content>
-    </React.Fragment>
+    <Switch>
+      <Route exact path={`${path}/home`} component={Home} />
+      <Route exact path={`${path}/calendar`} component={Calendar} />
+      <Route exact path={`${path}/analytics`} component={Analytics} />
+      <Route exact path={`${path}/leaderboard`} component={Leaderboard} />
+      <Route exact path={`${path}/my-account`} component={MyAccount} />
+      <Redirect to={`${path}/home`} />
+    </Switch>
   );
 };
 
