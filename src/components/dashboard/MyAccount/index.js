60a77420c039287a67e6588346a953abb8fa4fb4
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-06-23 11:28:06 +0530

    my account added

--- src/components/dashboard/MyAccount/index.js
+++ src/components/dashboard/MyAccount/index.js
@@ -1,36 +1,11 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
-import LoadingButton from "../../common/LoadingButton";
-import { useLogoutMutation } from "../../../app/services/hd/auth";
 import { useGetProfileQuery } from "../../../app/services/hd/school";
-import protectedHandler from "../../../utils/protectedHandler";
-import { removeSchoolAuth } from "../../../utils/schoolAuth";
+import Layout from "../../common/Layout";
 
 const MyAccount = () => {
-  const [logout, { isLoading: isLoggingOut }] = useLogoutMutation();
   const { data, isLoading } = useGetProfileQuery();
-  const history = useHistory();
 
-  const logoutSchool = protectedHandler(async () => {
-    await logout().unwrap();
-    removeSchoolAuth();
-    history.push("/auth/login");
-  });
-
-  return (
-    <div>
-      <LoadingButton
-        variant="contained"
-        color="primary"
-        onClick={logoutSchool}
-        isLoading={isLoggingOut}
-      >
-        Logout
-      </LoadingButton>
-      My Account
-      {isLoading ? "Loading" : data.email}
-    </div>
-  );
+  return <Layout>My Account - {isLoading ? "Loading" : data.email}</Layout>;
 };
 
 export default MyAccount;
