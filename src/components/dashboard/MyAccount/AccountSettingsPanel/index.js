cfd38c4eef5db207900893644c1decc12f372b3a
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-06-28 15:48:54 +0530

    account setting panel added

--- src/components/dashboard/MyAccount/AccountSettingsPanel/index.js
+++ src/components/dashboard/MyAccount/AccountSettingsPanel/index.js
@@ -1,7 +1,189 @@
-import React from "react";
+import React, { useState } from "react";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import EditIcon from "@material-ui/icons/Edit";
+import Avatar from "@material-ui/core/Avatar";
+import UpdatePassword from "../../../common/UpdatePassword";
+import UpdateContactNumber from "../../../common/UpdateContactNumber";
+import UpdateBoard from "../../../common/UpdateBoard";
+import UpdateAffiliationNumber from "../../../common/UpdateAffiliationNumber";
+import useStyles from "./style";
+import UpdateLogo from "../../../common/UpdateLogo";
+
+const EditableInfo = ({ value, onClick }) => {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.iconBox}>
+      <Typography>{value}</Typography>
+      <IconButton
+        color="primary"
+        onClick={onClick}
+        className={classes.iconButton}
+        aria-label="edit"
+      >
+        <EditIcon className={classes.editIcon} color="primary" />
+      </IconButton>
+    </Box>
+  );
+};
 
 const AccountSettingsPanel = () => {
-  return <div>AccountSettingsPanel</div>;
+  const classes = useStyles();
+
+  const initialState = {
+    password: false,
+    board: false,
+    contactNumber: false,
+    affiliationNumber: false,
+    logo: false,
+    coordinator: false,
+  };
+
+  const [open, setOpen] = useState(initialState);
+
+  const handleClickOpen = (id) => {
+    switch (id) {
+      case 0:
+        setOpen({ ...initialState, password: true });
+        break;
+      case 1:
+        setOpen({ ...initialState, board: true });
+        break;
+      case 2:
+        setOpen({ ...initialState, contactNumber: true });
+        break;
+      case 3:
+        setOpen({ ...initialState, affiliationNumber: true });
+        break;
+      case 4:
+        setOpen({ ...initialState, logo: true });
+        break;
+      case 5:
+        setOpen({ ...initialState, coordinator: true });
+        break;
+      default: // do nothing
+    }
+  };
+
+  const handleClose = () => {
+    setOpen(initialState);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Box className={classes.box}>
+        <Typography className={classes.name}>School name</Typography>
+        <Typography className={classes.value}>ABC Public School</Typography>
+      </Box>
+      <Box className={classes.box}>
+        <Typography className={classes.name}>Email address</Typography>
+        <Typography className={classes.value}>abc@gmail.com</Typography>
+      </Box>
+      <Box className={classes.editableBox}>
+        <Typography className={classes.name}>Password</Typography>
+        <EditableInfo
+          onClick={() => handleClickOpen(0)}
+          value={"***********"}
+        />
+      </Box>
+      <Typography color="primary" className={classes.title}>
+        School Details
+      </Typography>
+      <Box className={classes.box}>
+        <Typography className={classes.name}>School address</Typography>
+        <Typography className={classes.value}>abc, abc street</Typography>
+      </Box>
+      <Box className={classes.editableBox}>
+        <Typography className={classes.name}>Board</Typography>
+        <EditableInfo onClick={() => handleClickOpen(1)} value={"CBSE"} />
+      </Box>
+      <Box className={classes.editableBox}>
+        <Typography className={classes.name}>School Contact Number</Typography>
+        <EditableInfo onClick={() => handleClickOpen(2)} value={"123456789"} />
+      </Box>
+      <Box className={classes.editableBox}>
+        <Typography className={classes.name}>Affiliation number</Typography>
+        <EditableInfo onClick={() => handleClickOpen(3)} value={"12468153"} />
+      </Box>
+      <Box className={classes.box}>
+        <Typography className={classes.name}>Logo</Typography>
+        <div>
+          <Box className={classes.logoBox}>
+            <Avatar src={undefined} className={classes.logo}>
+              {""}
+            </Avatar>
+          </Box>
+          <Button
+            onClick={() => handleClickOpen(4)}
+            color="primary"
+            className={classes.logoChange}
+          >
+            Change
+          </Button>
+        </div>
+      </Box>
+      <Box className={classes.coordinatorTitle}>
+        <Typography color="primary" className={classes.title}>
+          School Coordinator Details
+        </Typography>
+        <IconButton
+          color="primary"
+          onClick={() => handleClickOpen(5)}
+          className={classes.titleButton}
+          aria-label="edit"
+        >
+          <EditIcon className={classes.editIcon} color="primary" />
+        </IconButton>
+      </Box>
+      <Box className={classes.box}>
+        <Typography className={classes.name}>Name</Typography>
+        <Typography className={classes.coordinatorValue}>Elon Musk</Typography>
+      </Box>
+      <Box className={classes.box}>
+        <Typography className={classes.name}>Designation</Typography>
+        <Typography className={classes.coordinatorValue}>Teacher</Typography>
+      </Box>
+      <Box className={classes.box}>
+        <Typography className={classes.name}>Contact Number</Typography>
+        <Typography className={classes.coordinatorValue}>123456789</Typography>
+      </Box>
+      <Box className={classes.box}>
+        <Typography className={classes.name}>Email address</Typography>
+        <Typography className={classes.coordinatorValue}>
+          xyz@gmail.com
+        </Typography>
+      </Box>
+      <UpdatePassword
+        open={open.password}
+        handleClose={handleClose}
+        aria-labelledby="update-password-dialog"
+      />
+      <UpdateBoard
+        open={open.board}
+        value={"CBSE"}
+        handleClose={handleClose}
+        aria-labelledby="update-board-dialog"
+      />
+      <UpdateContactNumber
+        open={open.contactNumber}
+        value={"123456789"}
+        handleClose={handleClose}
+        aria-labelledby="update-contact-number-dialog"
+      />
+      <UpdateAffiliationNumber
+        open={open.affiliationNumber}
+        value={"12468153"}
+        handleClose={handleClose}
+        aria-labelledby="update-affiliation-number-dialog"
+      />
+      <UpdateLogo  open={open.logo}
+        handleClose={handleClose}
+        aria-labelledby="update-logo-dialog"/>
+    </div>
+  );
 };
 
 export default AccountSettingsPanel;
--- src/components/dashboard/MyAccount/AccountSettingsPanel/style.js
+++ src/components/dashboard/MyAccount/AccountSettingsPanel/style.js
@@ -1,5 +1,87 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(5),
+  },
+  title: {
+    margin: theme.spacing(4, 0, 2),
+    fontSize: 18,
+    fontWeight: 600,
+  },
+  box: {
+    display: "flex",
+    justifyContent: "space-between",
+    border: "1px solid #C4C4C4",
+    padding: theme.spacing(2),
+  },
+  editableBox: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    border: "1px solid #C4C4C4",
+    paddingLeft: theme.spacing(2.5),
+  },
+  name: {
+    color: "#383838",
+    fontWeight: 600,
+    marginRight: theme.spacing(2),
+  },
+  iconBox: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  value: {
+    fontFamily: "'Inter', sans-serif",
+    color: "#1D1C3F",
+    marginRight: theme.spacing(5),
+  },
+  iconButton: {
+    height: 39,
+    width: 39,
+    margin: theme.spacing(1),
+  },
+  editIcon: {
+    fontSize: 15,
+  },
+  logoBox: {
+    marginTop: theme.spacing(2),
+    marginRight: theme.spacing(5),
+    border: "1px solid #C4C4C4",
+    height: 86,
+    width: 86,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    height: 72,
+    width: 72,
+    backgroundColor: "#C4C4C4",
+  },
+  logoChange: {
+    fontWeight: 600,
+    borderRadius: 40,
+    width: 86,
+    padding: theme.spacing(1, 2),
+    marginTop: theme.spacing(1),
+  },
+  coordinatorTitle: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  titleButton: {
+    margin: theme.spacing(3, 1, 2),
+    height: 39,
+    width: 39,
+  },
+  coordinatorValue: {
+    fontFamily: "'Inter', sans-serif",
+    color: "#1D1C3F",
+    marginRight: theme.spacing(1),
+  },
+}));
 
 export default useStyles;
