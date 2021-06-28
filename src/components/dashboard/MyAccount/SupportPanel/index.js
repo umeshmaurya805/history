2f23c34e2bbadc1241c436796ac90b858685c303
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-06-28 15:49:47 +0530

    support panel added

--- src/components/dashboard/MyAccount/SupportPanel/index.js
+++ src/components/dashboard/MyAccount/SupportPanel/index.js
@@ -1,7 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
+import TextField from "@material-ui/core/TextField";
+import { useFormik } from "formik";
+import LoadingButton from "../../../common/LoadingButton";
+import protectedHandler from "../../../../utils/protectedHandler";
+import useStyles from "./style";
+import { toast } from "react-toastify";
 
 const SupportPanel = () => {
-  return <div>SupportPanel</div>;
+  const classes = useStyles();
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [customTitle, setCustomTitle] = useState("");
+
+  const queryTitles = ["Technical", "Logical", "Serious", "Others"];
+  // const [login, { isLoading }] = useLoginMutation();
+
+  const formik = useFormik({
+    initialValues: {
+      title: queryTitles[0],
+      message: "",
+    },
+    onSubmit: protectedHandler(async (formData) => {
+      // await login(formData).unwrap();
+      console.log(formData);
+      toast.success("Message sent successfully",{ toastId: "SupportMessage"});
+    }),
+  });
+
+  const handleTitleSelectorChange = (index) => {
+    const title =
+      index !== queryTitles.length - 1 ? queryTitles[index] : customTitle;
+
+    setSelectedIndex(index);
+    formik.setFieldValue("title", title);
+  };
+
+  const handleTitleFieldChange = (e) => {
+    setCustomTitle(e.target.value)
+    formik.handleChange(e);
+  };
+
+  return (
+    <React.Fragment>
+      <Typography color="primary" className={classes.supportLabel}>
+        For any queries write to us
+      </Typography>
+      <Box>
+        {queryTitles.map((title, index) => {
+          return (
+            <Button
+              key={index}
+              variant={selectedIndex === index ? "contained" : "outlined"}
+              color={selectedIndex === index ? "primary" : undefined}
+              className={classes.titleButton}
+              onClick={() => handleTitleSelectorChange(index)}
+            >
+              {title}
+            </Button>
+          );
+        })}
+      </Box>
+      <form className={classes.form} onSubmit={formik.handleSubmit}>
+        <TextField
+          fullWidth
+          required
+          id="title"
+          name="title"
+          label="Title"
+          type="text"
+          variant="outlined"
+          margin="normal"
+          disabled={selectedIndex !== queryTitles.length - 1}
+          value={formik.values.title}
+          onChange={(e) => handleTitleFieldChange(e)}
+          error={formik.touched.title && Boolean(formik.errors.title)}
+          helperText={formik.touched.title && formik.errors.title}
+        />
+        <TextField
+          fullWidth
+          required
+          autoFocus
+          multiline
+          rows={7}
+          variant="outlined"
+          margin="normal"
+          id="message"
+          name="message"
+          label="Message"
+          placeholder="Type your message here"
+          value={formik.values.message}
+          onChange={formik.handleChange}
+          error={formik.touched.message && Boolean(formik.errors.message)}
+          helperText={formik.touched.message && formik.errors.message}
+        />
+        <LoadingButton
+          type="submit"
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+          // isLoading={isLoading}
+        >
+          Send
+        </LoadingButton>
+      </form>
+    </React.Fragment>
+  );
 };
 
 export default SupportPanel;
--- src/components/dashboard/MyAccount/SupportPanel/style.js
+++ src/components/dashboard/MyAccount/SupportPanel/style.js
@@ -1,5 +1,32 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  supportLabel: {
+    fontSize: 24,
+    fontWeight: 600,
+    margin: theme.spacing(8, 0, 2),
+  },
+  titleButton: {
+    width: 120,
+    marginRight: theme.spacing(5),
+    marginBottom: theme.spacing(2),
+    padding: theme.spacing(0.5, 3.5),
+    [theme.breakpoints.only("sm")]: {
+      marginRight: theme.spacing(3),
+    },
+    [theme.breakpoints.only("xs")]: {
+      marginRight: theme.spacing(2),
+    },
+  },
+  form: {
+    [theme.breakpoints.up("md")]: {
+      width: 650,
+    },
+  },
+  submit: {
+    marginTop: theme.spacing(2),
+    width: 120,
+  },
+}));
 
 export default useStyles;
