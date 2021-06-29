580b3300d63f92c24240372228a17467c9806be7
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-06-29 16:55:35 +0530

    Added event stats

---
+++ src/components/common/EventStats/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import StatsCard from "../StatsCard";
+
+const EventStats = () => {
+  const data = [
+    {
+      name: "Schools Partnered",
+      value: 56,
+    },
+    {
+      name: "Events Conducted",
+      value: 56,
+    },
+    { name: "Total Participation", value: 4567 },
+  ];
+
+  return (
+    <Grid container spacing={3}>
+      {data.map(({ name, value }) => {
+        return (
+          <Grid item style={{ flexGrow: 1 }}>
+            <StatsCard name={name} value={value} />
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+};
+
+export default EventStats;
