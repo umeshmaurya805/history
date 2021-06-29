b614d87ac55f8f97933980708e2a2acae65da408
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-06-29 16:57:13 +0530

    updated home

--- src/components/dashboard/Home/index.js
+++ src/components/dashboard/Home/index.js
@@ -1,9 +1,33 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
 import Layout from "../../common/Layout";
+import FeaturedEvent from "../../common/FeaturedEvent";
+import EventStats from "../../common/EventStats";
+import EventResult from "../../common/EventResult";
+import WelcomeBanner from "../../common/WelcomeBanner";
+import Magazine from "../../common/Magazine";
+import useStyles from "./style";
 
 const Home = () => {
-
-  return <Layout>Home</Layout>;
+  const classes = useStyles();
+  return (
+    <Layout>
+      <Container>
+        <WelcomeBanner />
+        <Grid container spacing={5} className={classes.resultStatsGrid}>
+          <Grid item lg={9} md={7} sm={6} xs={12}>
+            <EventStats />
+            <EventResult />
+          </Grid>
+          <Grid item lg={3} md={5} sm={6} xs={12}>
+            <Magazine />
+          </Grid>
+        </Grid>
+        <FeaturedEvent />
+      </Container>
+    </Layout>
+  );
 };
 
 export default Home;
