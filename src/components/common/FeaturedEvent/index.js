f4ea0824c0be363ded04f14a7f367a3c2632799c
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-06-29 16:56:00 +0530

    added featured event layout

---
+++ src/components/common/FeaturedEvent/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+
+const FeaturedEvent = () => {
+  // const classes = useStyles();
+
+  return (
+    <Grid container alignItems="center" spacing={4}>
+    
+    </Grid>
+  );
+};
+
+export default FeaturedEvent;
