28abb847b98fcb4b6bb0bc87744ca3550c7e6286
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-07-03 14:03:45 +0530

    update button group name change

---
+++ src/components/common/button/UpdateButtonGroup/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import SaveIcon from "@material-ui/icons/Save";
+import LoadingIconButton from "../LoadingIconButton";
+import useStyles from "./style";
+
+const UpdateButtonGroup = ({
+  isLoading,
+  handleOnClose,
+  handleOnSubmit,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Button
+        disableFocusRipple
+        className={classes.cancelButton}
+        onClick={handleOnClose}
+        color="primary"
+      >
+        Cancel
+      </Button>
+      <LoadingIconButton
+        type="submit"
+        onClick={handleOnSubmit}
+        className={classes.saveButton}
+        icon={<SaveIcon />}
+        color="primary"
+        variant="contained"
+        isLoading={isLoading}
+      >
+        Save
+      </LoadingIconButton>
+    </div>
+  );
+};
+
+export default UpdateButtonGroup;
