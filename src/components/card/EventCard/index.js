5fafd1ddaf73ec2ee1b4486ff92ac6c21466736f
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-07-06 19:56:52 +0530

    added event card

---
+++ src/components/card/EventCard/index.js
@@ -0,0 +1,74 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
+import useStyles from "./style";
+
+const EventCard = ({ data }) => {
+  const classes = useStyles();
+
+  const { title, subHeading, image, date, time, forClass } = data;
+
+  return (
+    <div className={classes.root}>
+      <Card>
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          className={classes.content}
+        >
+          <Box className={classes.imageContainer}>
+            <img src={image} alt="" className={classes.image} />
+          </Box>
+          <Typography variant="h4" className={classes.title}>
+            {title}
+          </Typography>
+          <Typography
+            display="block"
+            variant="body2"
+            className={classes.subHeading}
+          >
+            {subHeading}
+          </Typography>
+          <Box display="flex" alignItems="center" className={classes.infoBox}>
+            <Grid container justify="space-evenly">
+              <Box className={classes.innerBox}>
+                <Typography color="primary" className={classes.infoLabel}>
+                  Date
+                </Typography>
+                <Typography className={classes.infoValue}>{date}</Typography>
+              </Box>
+              <Box className={classes.innerBox}>
+                <Typography color="primary" className={classes.infoLabel}>
+                  Time
+                </Typography>
+                <Typography className={classes.infoValue}>{time}</Typography>
+              </Box>
+              <Box className={classes.innerBox}>
+                <Typography color="primary" className={classes.infoLabel}>
+                  Class
+                </Typography>
+                <Typography className={classes.infoValue}>
+                  {forClass}
+                </Typography>
+              </Box>
+            </Grid>
+          </Box>
+          <Button
+            className={classes.detailsButton}
+            size="small"
+            color="primary"
+            variant="outlined"
+          >
+            View Details
+          </Button>
+        </Box>
+      </Card>
+    </div>
+  );
+};
+
+export default EventCard;
