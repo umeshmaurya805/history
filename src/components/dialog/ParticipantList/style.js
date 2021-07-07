8b38d33124a170041387ac8fcde91389a8b47d47
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-07-07 18:43:19 +0530

    added participation list

---
+++ src/components/dialog/ParticipantList/style.js
@@ -0,0 +1,12 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  content: {
+    paddingBottom: theme.spacing(4),
+  },
+  box: {
+    marginBottom: theme.spacing(2),
+  },
+}));
+
+export default useStyles;
