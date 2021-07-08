ba524b1012adeb8228eb0889615ec3153e8bab3e
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-07-08 21:12:58 +0530

    event list dialog

---
+++ src/components/dashboard/Home/WelcomeBanner/style.js
@@ -0,0 +1,16 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  logo: {
+    height: 72,
+    width: 72,
+    backgroundColor: "#C4C4C4",
+  },
+  heading: {
+    fontFamily: "Roboto",
+    fontWeight: 700,
+    fontSize: 28,
+  },
+}));
+
+export default useStyles;
---
+++ src/components/dialog/EventList/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Typography from "@material-ui/core/Typography";
+
+const EventList = ({ date, data, handleClose, ...props }) => {
+  return (
+    <Dialog onClose={handleClose} {...props}>
+      <DialogTitle id="event-list-dialog-title" disableTypography>
+        Events on {date.toLocaleDateString()}
+      </DialogTitle>
+      <DialogContent>
+        <List aria-label="upcoming-event-list">
+          {data.map((event, index) => {
+            return (
+              <ListItem key={index} button onClick={() => handleClose(index)}>
+                <ListItemIcon>
+                  <div
+                    style={{
+                      borderRadius: "50%",
+                      backgroundColor: "blue",
+                      color: "white",
+                      height: 50,
+                      width: 50,
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      fontSize: 18,
+                      marginRight: "40px",
+                    }}
+                  >
+                    {event.startDate.getDate()}
+                  </div>
+                </ListItemIcon>
+                <ListItemText
+                  primary={event.name}
+                  secondary={
+                    <React.Fragment>
+                      <Typography component="span">{event.subHeading}</Typography>
+                      <Typography component="span">7:00 PM (IST)</Typography>
+                    </React.Fragment>
+                  }
+                />
+              </ListItem>
+            );
+          })}
+        </List>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default EventList;
