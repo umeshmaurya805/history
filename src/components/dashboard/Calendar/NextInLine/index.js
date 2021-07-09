fdb984dace898e4909f150660ec862179539f7d8
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-07-09 19:44:38 +0530

    next in line udpated

--- src/components/dashboard/Calendar/NextInLine/index.js
+++ src/components/dashboard/Calendar/NextInLine/index.js
@@ -3,10 +3,12 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
-import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import Chip from "@material-ui/core/Chip";
 import Typography from "@material-ui/core/Typography";
 import Title from "../../../common/Title";
 import useStyles from "./style";
+import { Box } from "@material-ui/core";
 
 const NextInLine = ({ events, handleEventClick }) => {
   const classes = useStyles();
@@ -16,31 +18,39 @@ const NextInLine = ({ events, handleEventClick }) => {
       <Title tooltipText="Events list for the current month">
         Next In Line
       </Title>
-      <List aria-label="next-in-line-list">
+      <List aria-label="next-in-line-list" className={classes.list}>
         {events.map((event, index) => {
           return (
-            <ListItem
-              key={index}
-              button
-              onClick={() => handleEventClick(index)}
-            >
-              <ListItemAvatar>
-                <Avatar className={classes.eventDate}>{event.startDate.getDate()}</Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary={event.name}
-                secondary={
-                  <React.Fragment>
-                    <Typography component="span">{event.subHeading}</Typography>
-
-                    <Typography component="span">
-                      {"                       "}
-                      {event.startDate.toLocaleTimeString()} (IST)
-                    </Typography>
-                  </React.Fragment>
-                }
-              />
-            </ListItem>
+            <React.Fragment key={index}>
+              <ListItem
+                selected={index === 0}
+                button
+                onClick={() => handleEventClick(index)}
+              >
+                <ListItemAvatar>
+                  <Avatar className={classes.eventDate}>
+                    {event.startDate.getDate()}
+                  </Avatar>
+                </ListItemAvatar>
+                <Box>
+                  <Typography component="p" className={classes.eventName}>
+                    {event.name}
+                  </Typography>
+                  <Typography component="p" className={classes.eventSubHeading}>
+                    {event.subHeading}
+                  </Typography>
+                  <Chip
+                    size="small"
+                    label={index % 3 === 0 ? "Not Registered" : "Registered"}
+                    className={classes.eventRegisteredTag}
+                    style={{
+                      background: index % 3 === 0 ? "#EF7373" : "#69DE91",
+                    }}
+                  />
+                </Box>
+              </ListItem>
+              {index + 1 !== events.length && <Divider />}
+            </React.Fragment>
           );
         })}
       </List>
--- src/components/dashboard/Calendar/NextInLine/style.js
+++ src/components/dashboard/Calendar/NextInLine/style.js
@@ -1,11 +1,38 @@
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
+  list: {
+    border: "1px solid #E5E5E5",
+    borderRadius: 12,
+    maxHeight: 360,
+    maxWidth: 429,
+    overflow: "auto",
+    boxShadow: "0px 5px 40px -10px rgba(0,0,0,0.14)",
+    "& .MuiListItem-root.Mui-selected, .MuiListItem-root.Mui-selected:hover": {
+      backgroundColor: "rgba(41, 121, 255, 0.08)",
+    },
+  },
   eventDate: {
     backgroundColor: "#007AFF",
     fontFamily: "Roboto",
     fontSize: 22,
     fontWeight: 900,
+    marginRight: 45,
+  },
+  eventName: {
+    fontFamily: "Roboto",
+    fontSize: 20,
+    fontWeight: 700,
+    color: theme.palette.primary.main,
+  },
+  eventSubHeading: {
+    fontSize: 12,
+    color: "#383838",
+  },
+  eventRegisteredTag: {
+    fontFamily: "Roboto",
+    margin: theme.spacing(1, 0),
+    padding: theme.spacing(0, 1),
   },
 }));
 
