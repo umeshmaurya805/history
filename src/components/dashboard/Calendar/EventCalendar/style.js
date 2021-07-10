02da55444d75ef8980fdc9706d1d77f80f46acc4
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-07-10 16:23:47 +0530

    calendar updated

--- src/components/dashboard/Calendar/EventCalendar/style.js
+++ src/components/dashboard/Calendar/EventCalendar/style.js
@@ -1,13 +1,18 @@
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  root: {},
+  root: {
+    [theme.breakpoints.only("xs")]: {
+      alignItems: "center",
+    },
+  },
   config: {
-    marginLeft:2,
+    width: "100%",
+    marginLeft: 2,
     marginBottom: theme.spacing(1),
     justifyContent: "space-between",
     "@media (max-width: 1050px)": {
-      maxWidth: 330,
+      // maxWidth: 330,
     },
     "& > div": {
       flexGrow: 1,
@@ -15,11 +20,31 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   calendar: {
+    width: "100%",
     color: "green !important",
     marginBottom: theme.spacing(4.5),
-    "@media (min-width: 1050px)": {
-      fontSize: 13,
+    border: "1px solid #E5E5E5",
+    boxShadow: "0px 5px 40px -10px rgba(0,0,0,0.14)",
+
+    "@media (min-width: 1600px)": {
+      fontSize: 16,
     },
+    "@media (min-width: 1400px) and (max-width: 1600px)": {
+      fontSize: 14,
+    },
+    "@media (min-width: 1200px) and (max-width: 1400px)": {
+      fontSize: 12,
+    },
+    "@media (min-width: 1050px) and (max-width: 1200px)": {
+      fontSize: 10,
+    },
+    "@media (min-width: 960px) and (max-width: 1050px)": {
+      fontSize: 8,
+    },
+    "@media (min-width: 600px) and (max-width: 960px)": {
+      fontSize: 10,
+    },
+
     "& .Calendar__yearText, & .Calendar__yearSelectorText, & .Calendar__monthText, & .Calendar__monthSelectorItemText":
       {
         color: "#333333",
--- src/components/dashboard/Calendar/index.js
+++ src/components/dashboard/Calendar/index.js
@@ -3,6 +3,8 @@ import Grid from "@material-ui/core/Grid";
 import Layout from "../../common/Layout";
 import EventCalendar from "./EventCalendar";
 import NextInLine from "./NextInLine";
+import EventDetails from "./EventDetails";
+import eventImage from "../../../assets/svg/event-image.png";
 import useStyles from "./style";
 // import { useParams } from 'react-router-dom';
 
@@ -11,60 +13,84 @@ const Calendar = () => {
 
   const events = [
     {
-      name: "Event 7 - World of Theater",
-      startDate: new Date(2021, 6, 28, 10),
-      endDate: new Date(2021, 7, 4, 20),
-      slug: "event-7",
-      subHeading: "This is our seventh event",
-    },
-    {
-      name: "Event 6",
-      startDate: new Date(2021, 6, 20, 10),
-      endDate: new Date(2021, 6, 20, 14, 30),
-      slug: "event-6",
-      subHeading: "This is our sixth event",
-    },
-    {
-      name: "Event 5",
-      startDate: new Date(2021, 6, 15, 10),
-      endDate: new Date(2021, 6, 16, 18, 30),
-      slug: "event-5",
-      subHeading: "This is our fifth event",
-    },
-    {
-      name: "Event 4",
-      startDate: new Date(2021, 6, 12, 12),
-      endDate: new Date(2021, 6, 18, 15),
-      slug: "event-4",
-      subHeading: "This is our fourth event",
-    },
-    {
-      name: "Event 3",
-      startDate: new Date(2021, 6, 9, 8),
-      endDate: new Date(2021, 6, 12, 14),
-      slug: "event-3",
-      subHeading: "This is our third event",
-    },
-    {
-      name: "Event 2",
-      startDate: new Date(2021, 6, 7, 12),
-      endDate: new Date(2021, 6, 10, 18, 30),
-      slug: "event-2",
-      subHeading: "This is our second event",
+      name: "Event X  Online theatre show for",
+      startDate: new Date(2021, 6, 1, 15),
+      endDate: new Date(2021, 6, 2, 18, 30),
+      slug: "event-0",
+      subHeading:
+        "This is our test event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
     },
     {
       name: "Event 1",
       startDate: new Date(2021, 6, 1, 10),
       endDate: new Date(2021, 6, 5, 18, 30),
       slug: "event-1",
-      subHeading: "This is our first event",
+      subHeading:
+        "This is our first event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
     },
     {
-      name: "Event 0",
-      startDate: new Date(2021, 6, 1, 15),
-      endDate: new Date(2021, 6, 2, 18, 30),
-      slug: "event-0",
-      subHeading: "This is our first event",
+      name: "Event 2",
+      startDate: new Date(2021, 6, 7, 12),
+      endDate: new Date(2021, 6, 10, 18, 30),
+      slug: "event-2",
+      subHeading:
+        "This is our second event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 3",
+      startDate: new Date(2021, 6, 9, 8),
+      endDate: new Date(2021, 6, 12, 14),
+      slug: "event-3",
+      subHeading:
+        "This is our third event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 4",
+      startDate: new Date(2021, 6, 12, 12),
+      endDate: new Date(2021, 6, 18, 15),
+      slug: "event-4",
+      subHeading:
+        "This is our fourth event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 5",
+      startDate: new Date(2021, 6, 15, 10),
+      endDate: new Date(2021, 6, 16, 18, 30),
+      slug: "event-5",
+      subHeading:
+        "This is our fifth event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 6",
+      startDate: new Date(2021, 6, 20, 10),
+      endDate: new Date(2021, 6, 20, 14, 30),
+      slug: "event-6",
+      subHeading:
+        "This is our sixth event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 7 - World of Theater",
+      startDate: new Date(2021, 6, 28, 10),
+      endDate: new Date(2021, 7, 4, 20),
+      slug: "event-7",
+      subHeading:
+        "This is our seventh event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
     },
   ];
 
@@ -72,19 +98,19 @@ const Calendar = () => {
 
   return (
     <Layout>
-      <Grid container>
-        <Grid item xs={12} lg={6}>
-          <Grid container justify="center">
-            <Grid item xs={12} sm={7} lg={12}>
+      <Grid container spacing={4}>
+        <Grid item xs={12} md={5} xl={4}>
+          <Grid container justify="center" spacing={3}>
+            <Grid item xs={12} sm={6} md={12}>
               <EventCalendar events={events} />
             </Grid>
-            <Grid item xs={12} sm={5} lg={12} className={classes.nextLine}>
+            <Grid item xs={12} sm={6} md={12} className={classes.nextLine}>
               <NextInLine events={events} handleEventClick={() => {}} />
             </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={12} lg={6}>
-          event
+        <Grid item xs={12} md={7} xl={8}>
+          <EventDetails event={events[0]} />
         </Grid>
       </Grid>
     </Layout>
