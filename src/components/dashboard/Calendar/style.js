a3a8b4cb997036159d7cf48d5e1d49a0f598a0e5
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-07-10 16:58:54 +0530

    update

--- src/components/dashboard/Calendar/style.js
+++ src/components/dashboard/Calendar/style.js
@@ -1,6 +1,10 @@
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
+  itemLeft: {
+    display: "flex",
+    alignItems:'center'
+  },
   nextLine: {
     "@media (max-width: 750px) and (min-width: 600px)": {
       maxWidth: "100%",
