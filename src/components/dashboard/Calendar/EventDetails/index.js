3dd416c7c5c9a04c2240db4778c4d578f0c925d6
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-07-10 16:23:58 +0530

    event details added

---
+++ src/components/dashboard/Calendar/EventDetails/index.js
@@ -0,0 +1,109 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import ReactMarkdown from "react-markdown";
+import useStyles from "./style";
+
+const EventDetails = ({ event }) => {
+  const classes = useStyles();
+
+  const { name, subHeading, image } = event;
+
+  const list = [
+    ["Date:", event.startDate.toLocaleDateString("in")],
+    ["Time:", event.startDate.toLocaleTimeString("in")],
+    ["Class:", "9 - 12"],
+    ["Mode:", "Online"],
+    ["Fee:", "Rs. 500"],
+    ["Registration Deadline:", new Date(2021, 5, 14).toLocaleDateString("in")],
+  ];
+
+  return (
+    <Grid container alignContent="space-between" className={classes.root}>
+      <Grid item xs={12} sm={6} md={12}>
+        <Box display="flex" flexDirection="column" alignItems="center">
+          <div className={classes.imageContainer}>
+            <img src={image} alt="" className={classes.image} />
+          </div>
+          <Typography
+            color="primary"
+            variant="h4"
+            className={classes.name}
+            gutterBottom
+          >
+            {name}
+          </Typography>
+          <Typography variant="body2" className={classes.subHeading}>
+            {subHeading}
+          </Typography>
+        </Box>
+        <Grid container>
+          {list.map((item, index) => {
+            return (
+              <Grid key={index} item xs={12}  sm={4} md={6} lg={4} xl={3}>
+                <Box display="flex" flexWrap="wrap" className={classes.item}>
+                  <Typography color="primary" className={classes.itemName}>
+                    {item[0]}
+                  </Typography>
+                  <Typography>{item[1]}</Typography>
+                </Box>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Grid>
+      <Grid item xs={12} sm={6} md={12}>
+        <ReactMarkdown
+          className={classes.reactMarkdown}
+          children={`Decription of event here .Decription of event here
+Decription of event here . Decription of event here. Decription of event here . Decription of event here. Decription of event here . Decription of event here Decription of event here . Decription of event here Decription of event here .Decription of event here Decription of event here .Decription of event here Decription of event here 
+
+Decription of event here .Decription of event here Decription of event here .Decription of event here Decription of event here Decription of event here Decription of event here .Decription of event here Decription of event here .Decription of event here
+Decription of event here .Decription of event here
+Decription of event here . Decription of event here. Decription of event here . Decription of event here. Decription of event here . Decription of event here Decription of event here . Decription of event here Decription of event here .Decription of event here Decription of event here .Decription of event here Decription of event here 
+
+Decription of event here .Decription of event here Decription of event here .Decription of event here Decription of event here Decription of event here Decription of event here .Decription of event here Decription of event here .Decription of event here
+Decription of event here .Decription of event here
+Decription of event here . Decription of event here. Decription of event here . Decription of event here. Decription of event here . Decription of event here Decription of event here . Decription of event here Decription of event here .Decription of event here Decription of event here .Decription of event here Decription of event here 
+
+Decription of event here .Decription of event here Decription of event here .Decription of event here Decription of event here Decription of event here Decription of event here .Decription of event here Decription of event here .Decription of event here
+Decription of event here .Decription of event here
+Decription of event here . Decription of event here. Decription of event here . Decription of event here. Decription of event here . Decription of event here Decription of event here . Decription of event here Decription of event here .Decription of event here Decription of event here .Decription of event here Decription of event here 
+
+Decription of event here .Decription of event here Decription of event here .Decription of event here Decription of event here Decription of event here Decription of event here .Decription of event here Decription of event here .Decription of event here
+Decription of event here .Decription of event here
+Decription of event here . Decription of event here. Decription of event here . Decription of event here. Decription of event here . Decription of event here Decription of event here . Decription of event here Decription of event here .Decription of event here Decription of event here .Decription of event here Decription of event here 
+
+Decription of event here .Decription of event here Decription of event here .Decription of event here Decription of event here Decription of event here Decription of event here .Decription of event here Decription of event here .Decription of event here
+Decription of event here .Decription of event here
+Decription of event here . Decription of event here. Decription of event here . Decription of event here. Decription of event here . Decription of event here Decription of event here . Decription of event here Decription of event here .Decription of event here Decription of event here .Decription of event here Decription of event here 
+
+Decription of event here .Decription of event here Decription of event here .Decription of event here Decription of event here Decription of event here Decription of event here .Decription of event here Decription of event here .Decription of event here
+`}
+        />
+      </Grid>
+      <Grid item xs={12} sm={6} md={12}>
+        <Box display="flex" justifyContent="space-evenly">
+          <Button
+            variant="contained"
+            color="secondary"
+            className={classes.messageButton}
+          >
+            Student Message
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.registerButton}
+          >
+            Cancel Registration
+          </Button>
+        </Box>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default EventDetails;
---
+++ src/components/dashboard/Calendar/EventDetails/style.js
@@ -0,0 +1,85 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    // marginTop: theme.spacing(8),
+    height: "100%",
+    border: "1px solid #E5E5E5",
+    borderRadius: 12,
+    boxShadow: "0px 5px 40px -10px rgba(0,0,0,0.14)",
+    padding: theme.spacing(5),
+
+    "@media (max-width: 1100px)": {
+      "& > div": {
+        maxWidth: "100%",
+        flexBasis: "100%",
+      },
+    },
+  },
+  imageContainer: {
+    width: "100%",
+    paddingTop: "50%" /* 2:1 Aspect Ratio */,
+    position: "relative",
+  },
+  image: {
+    width: "100%",
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    borderRadius: 12,
+    boxShadow: "0px 5px 10px rgba(0,0,0,0.24)",
+    marginBottom: theme.spacing(2),
+  },
+  name: {
+    marginTop: 70,
+    fontFamily: "Roboto",
+    fontWeight: 900,
+    fontSize: 22,
+    textAlign: "center",
+    "@media (min-width: 1700px)": {
+      marginTop: 100,
+    },
+  },
+  subHeading: {
+    color: "#383838",
+    fontFamily: "Roboto",
+    textAlign: "center",
+    marginBottom: theme.spacing(3),
+  },
+  item: {
+    margin: theme.spacing(1, 2),
+    minWidth: 105,
+  },
+  itemName: {
+    fontFamily: "Roboto",
+    fontWeight: 500,
+    marginRight: theme.spacing(1),
+  },
+  reactMarkdown: {
+    maxHeight: 400,
+    overflow: "auto",
+    margin: theme.spacing(2, 0,4),
+
+    "@media (min-width: 1700px)": {
+      maxHeight: 230,
+    },
+  },
+  messageButton: {
+    backgroundColor: "#F89503",
+    color: "white",
+    width: 180,
+    "&:hover": {
+      backgroundColor: "#C37603",
+    },
+    "&:active": {
+      backgroundColor: "#C37603",
+    },
+  },
+  registerButton: {
+    width: 180,
+  },
+}));
+
+export default useStyles;
