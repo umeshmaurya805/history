6653c837e81d341d701d8ddbe6e12e399ffb16dd
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-07-11 15:03:29 +0530

    confirmation dialog added

---
+++ src/components/dialog/ConfirmationDialog/index.js/index.js
@@ -0,0 +1,63 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import Typography from "@material-ui/core/Typography";
+import CloseIcon from "@material-ui/icons/Close";
+import useStyles from "./style";
+
+const ConfirmationDialog = ({
+  title,
+  content,
+  handleOk,
+  handleCancel,
+  ...props
+}) => {
+  const classes = useStyles();
+
+  return (
+    <Dialog onClose={handleCancel} {...props}>
+      <DialogTitle
+        id="confirmation-dialog-title"
+        disableTypography
+        className={classes.dialogTitle}
+      >
+        <Typography className={classes.title}> {title} </Typography>
+        <IconButton
+          edge="start"
+          color="inherit"
+          onClick={handleCancel}
+          aria-label="close"
+        >
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent>
+        <Typography className={classes.content}>{content}</Typography>
+      </DialogContent>
+      <DialogActions className={classes.dialogActions}>
+        <Button
+          variant="contained"
+          color="primary"
+          className={classes.button}
+          onClick={handleCancel}
+        >
+          No
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          className={classes.button}
+          onClick={handleOk}
+        >
+          Yes
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ConfirmationDialog;
---
+++ src/components/dialog/ConfirmationDialog/index.js/style.js
@@ -0,0 +1,33 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  dialogTitle: {
+    display: "flex",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 700,
+    margin: "auto",
+  },
+  content: {
+    fontSize: 18,
+    fontWeight: 600,
+    maxWidth: 400,
+    textAlign: "center",
+    margin: theme.spacing(1, 3),
+    "& > span": {
+      color: theme.palette.primary.main,
+    },
+  },
+  dialogActions: {
+    justifyContent: "space-between",
+    margin: theme.spacing(0, 3, 2),
+  },
+  button: {
+    width: 120,
+    fontFamily: "Roboto",
+    fontSize: 18,
+  },
+}));
+
+export default useStyles;
