8613e810033e48b86d452ccbee7df9dde3f06afc
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-07-11 15:27:36 +0530

    changes

---
+++ src/utils/algorithms.js
@@ -0,0 +1,32 @@
+import { compareAsc } from "date-fns";
+
+export const eventBinarySearch = (events, dateToCompare) => {
+  // let low = 0,
+  //   high = events.length - 1,
+  //   res;
+
+  // while (low <= high) {
+  //   const mid = Math.floor(low + (high - low) / 2);
+
+  //   const { slug, startDate } = events[mid];
+
+  //   const cmp = compareAsc(dateToCompare, startDate);
+
+  //   switch (cmp) {
+  //     case 0:
+  //       return slug;
+  //     case -1:
+  //       res = slug;
+  //       high = mid - 1;
+  //       break;
+
+  //     case 1:
+  //       low = mid + 1;
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  // }
+
+  // return res;
+};
