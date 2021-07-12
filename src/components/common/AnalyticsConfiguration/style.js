6377d68b2421c75d516f255fe250a35d9a227221
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-07-12 18:07:59 +0530

    analytics configuration added

---
+++ src/components/common/AnalyticsConfiguration/style.js
@@ -0,0 +1,66 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(5),
+  },
+  gridItem: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  selector: {
+    [theme.breakpoints.only("xs")]: {
+      flexGrow: 1,
+    },
+  },
+  yearSelector: {
+    [theme.breakpoints.only("xs")]: {
+      width: "100%",
+    },
+  },
+  class: {
+    minWidth: 47,
+    border: "1px solid #E5E5E5",
+  },
+  user: {
+    minWidth: 48,
+    border: "1px solid #E5E5E5",
+  },
+  category: {
+    minWidth: 125,
+    [theme.breakpoints.only("xs")]: {
+      width: "100%",
+      marginLeft: "unset",
+    },
+  },
+  pastDays: {
+    minWidth: 80,
+    border: "1px solid #E5E5E5",
+    marginLeft: theme.spacing(1),
+  },
+  academicYear: {
+    minWidth: 60,
+    marginLeft: theme.spacing(1),
+    border: "1px solid #E5E5E5",
+  },
+  customRangeContainer: {
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+  buttonContainer: {
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+  button: {
+    margin: theme.spacing(1),
+    width: 90,
+    boxShadow: "0px 5px 8px -3px rgba(0,0,0,0.14)",
+    borderRadius: 12,
+
+    "&.MuiButton-outlined": {
+      border: "1px solid #E5E5E5",
+    },
+  },
+}));
+
+export default useStyles;
