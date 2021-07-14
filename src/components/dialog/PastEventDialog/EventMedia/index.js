74b07dab15bc8fdeacd502773f03a56e145ee77a
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-07-14 16:06:32 +0530

    event media initialised

---
+++ src/components/dialog/PastEventDialog/EventMedia/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import useStyles from './style'
+
+const EventMedia = () => {
+    const classes = useStyles()
+    return (
+        <div className={classes.root}>
+            EventMedia
+        </div>
+    )
+}
+
+export default EventMedia
