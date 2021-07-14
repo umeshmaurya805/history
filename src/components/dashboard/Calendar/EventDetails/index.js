d615d7ca2ef49eb6e31469e492aa21b817719cb9
Divyansh Singh Thakur <divyansh2140@gmail.com>
2021-07-14 16:08:29 +0530

    next in line and event details updated

--- src/components/dashboard/Calendar/EventDetails/index.js
+++ src/components/dashboard/Calendar/EventDetails/index.js
@@ -1,21 +1,130 @@
 import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import ReactMarkdown from "react-markdown";
-import useStyles from "./style";
+import eventImage from "../../../../assets/svg/event-image.png";
 import StudentMessageDialog from "../../../dialog/StudentMessageDialog";
 import ConfirmationDialog from "../../../dialog/ConfirmationDialog";
+import useStyles from "./style";
 
-const EventDetails = ({ events }) => {
+const EventDetails = () => {
   const classes = useStyles();
+  const { slug } = useParams();
 
-  const { name, subHeading, image, startDate } = events[0];
+  const events = [
+    {
+      name: "Event X  Online theatre show",
+      startDate: new Date(2021, 6, 1, 15),
+      endDate: new Date(2021, 6, 2, 18, 30),
+      slug: "event-0",
+      subHeading:
+        "This is our test event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 1",
+      startDate: new Date(2021, 6, 1, 10),
+      endDate: new Date(2021, 6, 5, 18, 30),
+      slug: "event-1",
+      subHeading:
+        "This is our first event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 2",
+      startDate: new Date(2021, 6, 7, 12),
+      endDate: new Date(2021, 6, 10, 18, 30),
+      slug: "event-2",
+      subHeading:
+        "This is our second event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 3",
+      startDate: new Date(2021, 6, 9, 8),
+      endDate: new Date(2021, 6, 12, 14),
+      slug: "event-3",
+      subHeading:
+        "This is our third event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 4",
+      startDate: new Date(2021, 6, 12, 12),
+      endDate: new Date(2021, 6, 18, 15),
+      slug: "event-4",
+      subHeading:
+        "This is our fourth event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 5",
+      startDate: new Date(2021, 6, 15, 10),
+      endDate: new Date(2021, 6, 16, 18, 30),
+      slug: "event-5",
+      subHeading:
+        "This is our fifth event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 6",
+      startDate: new Date(2021, 6, 20, 10),
+      endDate: new Date(2021, 6, 20, 14, 30),
+      slug: "event-6",
+      subHeading:
+        "This is our sixth event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 7 - World of Theater",
+      startDate: new Date(2021, 6, 28, 10),
+      endDate: new Date(2021, 7, 4, 20),
+      slug: "event-7",
+      subHeading:
+        "This is our seventh event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 8 - World of Theater",
+      startDate: new Date(2021, 6, 28, 8),
+      endDate: new Date(2021, 7, 7, 15),
+      slug: "event-8",
+      subHeading:
+        "This is our eight event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 9 - World of Theater",
+      startDate: new Date(2021, 6, 28, 12),
+      endDate: new Date(2021, 7, 1, 15),
+      slug: "event-9",
+      subHeading:
+        "This is our ninth event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+  ];
+
+  const event = events.find((event) => event.slug === slug);
+
+  console.log("event", event);
+  const { name, subHeading, image, startDate } = event;
 
   const list = [
-    ["Date:", startDate.toLocaleDateString("in")],
-    ["Time:", startDate.toLocaleTimeString("in")],
+    ["Date:", startDate?.toLocaleDateString("in")],
+    ["Time:", startDate?.toLocaleTimeString("in")],
     ["Class:", "9 - 12"],
     ["Mode:", "Online"],
     ["Fee:", "Rs. 500"],
--- src/components/dashboard/Calendar/NextInLine/index.js
+++ src/components/dashboard/Calendar/NextInLine/index.js
@@ -6,12 +6,119 @@ import Avatar from "@material-ui/core/Avatar";
 import Divider from "@material-ui/core/Divider";
 import Chip from "@material-ui/core/Chip";
 import Typography from "@material-ui/core/Typography";
-import Title from "../../../common/Title";
-import useStyles from "./style";
 import { Box } from "@material-ui/core";
+import { useHistory, useParams } from "react-router-dom";
+import Title from "../../../common/Title";
+import eventImage from "../../../../assets/svg/event-image.png";
+import useStyles from "./style";
 
-const NextInLine = ({ events, handleEventClick }) => {
+const NextInLine = () => {
   const classes = useStyles();
+  const history = useHistory();
+  const { slug } = useParams();
+
+  const events = [
+    {
+      name: "Event X  Online theatre show",
+      startDate: new Date(2021, 6, 1, 15),
+      endDate: new Date(2021, 6, 2, 18, 30),
+      slug: "event-0",
+      subHeading:
+        "This is our test event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 1",
+      startDate: new Date(2021, 6, 1, 10),
+      endDate: new Date(2021, 6, 5, 18, 30),
+      slug: "event-1",
+      subHeading:
+        "This is our first event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 2",
+      startDate: new Date(2021, 6, 7, 12),
+      endDate: new Date(2021, 6, 10, 18, 30),
+      slug: "event-2",
+      subHeading:
+        "This is our second event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 3",
+      startDate: new Date(2021, 6, 9, 8),
+      endDate: new Date(2021, 6, 12, 14),
+      slug: "event-3",
+      subHeading:
+        "This is our third event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 4",
+      startDate: new Date(2021, 6, 12, 12),
+      endDate: new Date(2021, 6, 18, 15),
+      slug: "event-4",
+      subHeading:
+        "This is our fourth event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 5",
+      startDate: new Date(2021, 6, 15, 10),
+      endDate: new Date(2021, 6, 16, 18, 30),
+      slug: "event-5",
+      subHeading:
+        "This is our fifth event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 6",
+      startDate: new Date(2021, 6, 20, 10),
+      endDate: new Date(2021, 6, 20, 14, 30),
+      slug: "event-6",
+      subHeading:
+        "This is our sixth event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 7 - World of Theater",
+      startDate: new Date(2021, 6, 28, 10),
+      endDate: new Date(2021, 7, 4, 20),
+      slug: "event-7",
+      subHeading:
+        "This is our seventh event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 8 - World of Theater",
+      startDate: new Date(2021, 6, 28, 8),
+      endDate: new Date(2021, 7, 7, 15),
+      slug: "event-8",
+      subHeading:
+        "This is our eight event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+    {
+      name: "Event 9 - World of Theater",
+      startDate: new Date(2021, 6, 28, 12),
+      endDate: new Date(2021, 7, 1, 15),
+      slug: "event-9",
+      subHeading:
+        "This is our ninth event Online theatre show for class 9-12  Online theatre show for class 9-12",
+      image: eventImage,
+      description: "Event details, rounds timings, etc.",
+    },
+  ];
 
   return (
     <div className={classes.root}>
@@ -23,9 +130,12 @@ const NextInLine = ({ events, handleEventClick }) => {
           return (
             <React.Fragment key={index}>
               <ListItem
-                selected={index === 0}
+                selected={event.slug === slug}
+                // autoFocus={event.slug === slug}
                 button
-                onClick={() => handleEventClick(index)}
+                onClick={() =>
+                  history.push(`/dashboard/calendar/${event.slug}`)
+                }
               >
                 <ListItemAvatar>
                   <Avatar className={classes.eventDate}>
